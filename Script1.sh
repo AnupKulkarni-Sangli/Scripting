bc4983b55b3a16c215e4cad1e0180bbe85a66b8e
Anup Kulkarni <akcool1680@gmail.com>
2026-09-25 18:40:17 +0530

    I am performing a practical on Git, Thanks

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-33 and learning git/gihub"
 echo "We are learning Branching"
+echo "I have completed Docker, Git and Ansible practicals"
+echo "I need to prioritize for creation of a resume and prepare for interviews"
 
