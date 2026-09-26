06cc7cf8f3e82bc69d7a68898685c866f4f06cde
Anup Kulkarni <akcool1680@gmail.com>
2026-09-26 09:35:42 +0530

    This is my commit related to resume preparation, Thanks

--- Script1.sh
+++ Script1.sh
@@ -3,5 +3,5 @@ echo "We are from batch-33 and learning git/gihub"
 echo "We are learning Branching"
 echo "I have completed Docker, Git and Ansible practicals"
 echo "Thank you Young-minds for the support and guidance"
-echo "I need to Learn more about Kubernets and Jenkins"
+echo "I need to create my Devops resume and start applying for jobs"
 
